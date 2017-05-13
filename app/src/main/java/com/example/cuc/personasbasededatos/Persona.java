0c7c41af1819e1e7c4979bb89b1232a8b98d4fb0
Leiner Escorcia <leiner_0304@hotmail.com>
2017-05-12 21:22:08 -0500

    Interfaz personas terminada 1.1

---
+++ app/src/main/java/com/example/cuc/personasbasededatos/Persona.java
@@ -0,0 +1,66 @@
+package com.example.cuc.personasbasededatos;
+
+/**
+ * Created by CUC on 12/05/2017.
+ */
+
+public class Persona {
+    private String foto, cedula, nombre, apellido, sexo, pasatiempo;
+
+    public Persona(String foto, String cedula, String nombre, String apellido, String sexo, String pasatiempo) {
+        this.foto = foto;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+        this.pasatiempo = pasatiempo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPasatiempo() {
+        return pasatiempo;
+    }
+
+    public void setPasatiempo(String pasatiempo) {
+        this.pasatiempo = pasatiempo;
+    } 
+}
